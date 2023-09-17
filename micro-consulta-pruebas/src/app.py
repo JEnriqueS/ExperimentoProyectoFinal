6f09613a9d231efae0e14b5464a04739961bdc97
Jorge Enrique Suárez González <je.suarezg1@uniandes.edu.co>
2023-09-17 15:30:02 -0500

    Ajustes conexión BD Mysql

--- micro-consulta-pruebas/src/app.py
+++ micro-consulta-pruebas/src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_restful import Api
 from src.modelos import db
 from src.vistas import (VistaConsultaPrueba)
+import os
+sqlpass = os.getenv("SQL_PASSWORD")
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eporra.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:'+sqlpass+'@34.71.192.222:3306/candidatos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
@@ -14,7 +16,9 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-cors = CORS(app)
-
 api = Api(app)
 api.add_resource(VistaConsultaPrueba, '/prueba_tecnica/<int:id_prueba>')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
